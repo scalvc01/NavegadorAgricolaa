80de79933ad88065a17ecc39f950b16dccd49061
scalvc01 <scalvc01@estudiantes.unileon.es>
2019-01-28 13:28:29 +0100

    Initial commit

---
+++ app/src/main/java/example/s_rus/navegadoragricola/Fragment05.java
@@ -0,0 +1,18 @@
+package example.s_rus.navegadoragricola;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment05 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment05, container, false);
+    }
+}
